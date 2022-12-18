5a357d53b6b5ec4b7a38a0fc0efeddc102a7c25b
Abdalla Ibrahim <abdullahibrahim544@gmail.com>
2022-12-18 22:05:35 +0200

    added compression prototype

--- operations.h
+++ operations.h
@@ -2,9 +2,14 @@
 #ifndef DS_PROJECT_OPERATIONS_H
 #define DS_PROJECT_OPERATIONS_H
 #include "algorithm"
-#include "string.h"
+#include <cstring>
 #include <iostream>
 #include <stack>
+#include "fstream"
+#include "sstream"
+#include "HashMap.h"
+#include "vector"
 using namespace std;
 string minify(string test);
+void compress(string text);
 #endif //DS_PROJECT_OPERATIONS_H
