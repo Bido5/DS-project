47ea61e0a15123d3b7f30bb98e843a12d13a4357
Bahnasy2001 <105355425+Bahnasy2001@users.noreply.github.com>
2022-12-19 20:06:25 +0200

    Update formatter.cpp

--- formatter.cpp
+++ formatter.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 string insert_tabs(string s, int n)
 {
-    string insert_tabs;
+	string insert_tabs;
 	for (int i = 0; i < n; i++)
 		insert_tabs += s;
 
@@ -17,40 +17,45 @@ string insert_tabs(string s, int n)
 
 void format_xml(string* file)
 {
-    queue <string> q;
-    string x = "";
-    string s = "";
-    string t("    ");
+	queue <string> q;
+	string x = "";
+	string s = "";
+	string t("    ");
 	string n("\n");
-	long long i = 0, flag;//flag for the number of characters in tags and data
-	int flag_tabs = 1;  //flag for the number of tabs
-	while (i != file->size())
-    {
-        flag=0;
-        if (file->at(i) == '<')
+	long long i = 0, count;//count for the number of characters in tags and data
+	int count_tabs = 1;  //count for the number of tabs
+	while (i < file->length())
+	{
+		count = 0;
+		if (file->at(i) == '<')
 		{
+			if (file->at(i) == '/') {
+
+			}
 			for (long x = i; file->at(x) != '>'; x++)
 			{
-				flag++;
+				count++;
 			}
-			flag++;
-			x = file->substr(i, flag);
+			count++;
+			x = file->substr(i, count);
 			q.push(x);     //pushing opening and closing tag into the queue
-			i = i + flag;
+			i = i + count;
 		}
 		else
 		{
+			
 			for (long x = i; file->at(x) != '<'; x++)
 			{
-				flag++;
+				count++;
 			}
-			x = file->substr(i, flag);
+			
+			x = file->substr(i, count);
 			q.push(x);    //pushing data into the queue
-			i += flag;
+			i += count;
 		}
 
 	}
-    while (!q.empty())
+	while (!q.empty())
 	{
 		x = q.front();
 		if (x.at(0) == '<')
@@ -58,37 +63,45 @@ void format_xml(string* file)
 			//closing tags
 			if (x.at(1) == '/')
 			{
-				flag_tabs-=2;
-				s += insert_tabs(t, flag_tabs);
+				count_tabs -= 2;
+				s += insert_tabs(t, count_tabs);
 				s = s + x;
 				s = s + n;
 				q.pop();
 				if (!q.empty())
 				{
-					if (q.front().at(1) != '/')
-					{
-						s += insert_tabs(t, flag_tabs);
-						flag_tabs++;
-					}
-					else
-					{
-						flag_tabs++;
-					}
+					// to fix the problem of /n without spaces
+					if (q.front().length() > 1) {
+						if (q.front().at(1) != '/')
+						{
+
+							s += insert_tabs(t, count_tabs);
+							count_tabs++;
+							
+						}
+						else
+						{
+							count_tabs++;
+						}
+					} 
+					
 				}
 			}
 			//opening tag
 			else
 			{
+				
 				s = s + x;
 				s = s + n;
-				s += insert_tabs(t, flag_tabs);
-				flag_tabs++;
+				s += insert_tabs(t, count_tabs);
+				count_tabs++;
 				q.pop();
 			}
 		}
 		//data
 		else
 		{
+			
 			s = s + x;
 			s = s + n;
 			q.pop();
