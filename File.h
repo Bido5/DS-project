0fead717aa151208601f21f5a1af5ff152f1034d
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2022-12-26 13:48:41 +0200

    Update File.h

--- File.h
+++ File.h
@@ -1,24 +1,30 @@
+#pragma once
 #ifndef FILE_H
 #define FILE_H
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
+
 using namespace std;
 
 class File
 {
     private:
-        fstream xfile;
-        vector <string> elements;
-        string fileName = "resources/sample.txt";
+    fstream xfile;
+    vector <string> elements;
+    string fileName;
 
 
     public:
-        File();
-        virtual ~File();
-        void toArray();
-        vector <string> getElements();
+    File(string fileName);
+    virtual ~File();
+    void toArray();
+    vector <string> getElements();
+    void saveFile(string text);
+    
+    string exportFile();
+    
 };
 
 #endif // FILE_H
