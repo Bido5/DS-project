6547e7ee6177358669fe9c66657e688b1992488c
Heba2h <99084258+Heba2h@users.noreply.github.com>
2023-01-29 10:43:39 +0200

    fix: some features

--- DS_project/main.cpp
+++ DS_project/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+
 using namespace std;
 
 void checker(string str,string tag,int start_subStr , int end_subStr,bool flag_error, int line , stack<pair<string,int>> stack ) ;
 
-
-int main() {
-    string str;
-    freopen("sample.in", "r", stdin);
+vector <string> Consistency (string str){
     int end_subStr = 0;
     stack<pair<string, int>> st;
     int line = 1;
@@ -15,6 +14,7 @@ int main() {
     string close_tag;
     bool flag_error;
     stack<pair<string, int>> error_stack;
+    vector <int > output ;
 
     while (getline(cin, str)) {
 
@@ -25,8 +25,7 @@ int main() {
 
         /*checking if any of token is missed*/
         if ((start_subStr == -1 && end_subStr != -1) || (start_subStr != -1 && end_subStr == -1)) {
-            cout << "Missing token";
-            cout << " in line: " << line << endl;
+            cout << "Missing token in line: " << line << endl;
             start_subStr = 0, end_subStr = 0;
             line++;
             continue;
@@ -38,12 +37,10 @@ int main() {
             else if ((flag_error && tag.find('<') != string::npos) && end_subStr != -1 &&
                      tag[tag.find('<') + 1] != '/') {
                 st.push({tag.substr(tag.find('<') + 1, end_subStr - start_subStr - 1), (line)});
-                cout << "Missing token _!";
-                cout << "in line: " << line << endl;
+                cout << "Missing token in line: " << line << endl;
 
             } else {
-                cout << "Missing token!!";
-                cout << " in line: " << line << endl;
+                cout << "Missing token in line: " << line << endl;
             }
         }
         if ((str[start_subStr]) == '<' && str[start_subStr + 1] == '/') {
@@ -87,8 +84,7 @@ int main() {
             flag_error = (tag.find('<') != string::npos || tag.find('>') != string::npos || end_subStr == -1);
 
             if ((start_subStr == -1 && end_subStr != -1) || (start_subStr != -1 && end_subStr == -1)) {
-                cout << "Missing token";
-                cout << "in line: " << line << endl;
+                cout << "Missing token in line: " << line << endl;
                 start_subStr = 0, end_subStr = 0;
                 line++;
                 continue;
@@ -101,12 +97,10 @@ int main() {
                 else if ((flag_error && tag.find('<') != string::npos) && end_subStr != -1 &&
                          tag[tag.find('<') + 1] != '/') {
                     st.push({tag.substr(tag.find('<') + 1, end_subStr - start_subStr - 1), (line)});
-                    cout << "Missing token _!";
-                    cout << "in line: " << line << endl;
+                    cout << "Missing token in line: " << line << endl;
 
                 } else {
-                    cout << "Missing token!!";
-                    cout << "in line: " << line << endl;
+                    cout << "Missing token in line: " << line << endl;
                 }
             }
             if ((str[start_subStr]) == '<' && str[start_subStr + 1] == '/') {
@@ -145,13 +139,20 @@ int main() {
         line++;
     }
 
+    vector <string>  Errors;
+//   if flag == true;
 
     while (!error_stack.empty()) {
-        cout << "Missing closing tag -> " << error_stack.top().first << " in line: " << error_stack.top().second
-             << endl;
+
+        Errors.push_back("Missing closing tag -> " + error_stack.top().first + " in line: " + to_string(error_stack.top().second)+"\n");
         error_stack.pop();
     }
-
+    int g =0;
+    while (g<Errors.size()) {
+        cout << Errors.at(g);
+        g++;
+    }
+    return Errors;
 
     while (!st.empty()) {
 
@@ -159,6 +160,28 @@ int main() {
         cout << "  The top of the stack in is : " << st.top().first << endl;
         st.pop();
     }
+}
+
+vector <string> correction (string str )
+{
+    
+
+
+}
+
+
+
+
+int main() {
+    string str;
+    freopen("sample.in", "r", stdin);
+
+    Consistency(str);
+
+
+
+
+
     return 0;
 }
 
