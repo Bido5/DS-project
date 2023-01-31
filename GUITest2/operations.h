634490bf06b4c5c207bbaa05af4979791cd1287d
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2023-01-31 14:46:39 +0200

    Update operations.h

--- GUITest2/operations.h
+++ GUITest2/operations.h
@@ -10,14 +10,24 @@
 #include "sstream"
 #include "HashMap.h"
 #include "vector"
+#include <queue>
+#include "File.h"
 
 
 
 using namespace std;
-string minify(string text);
-string minify(vector <string> text);
-//vector<int> compress(string text, HashMaphash);
-string decompress(vector<int> compressed, HashMap* hash);
-vector <string> to_array(string xml);
-//vector <string> Consistency(string input_stream);
+
+static class XmlOp {
+public:
+	static queue <string> to_queue(string xml);
+	static string minify(string text);
+	static string minify(vector <string> text);
+	//vector<int> compress(string text, HashMaphash);
+	static string decompress(vector<int> compressed, HashMap* hash);
+	static vector <string> to_array(string xml);
+	static string format_xml(string file);
+private:
+	static string XmlOp::insert_tabs(char s, int n);
+	//vector <string> Consistency(string input_stream);
+};
 #endif // DS_PROJECT_OPERATIONS_H
