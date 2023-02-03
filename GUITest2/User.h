8f5d30a79622e49ba6eccf75adcc9b80a45143d7
Sar00-Ahmed <104655280+Sar00-Ahmed@users.noreply.github.com>
2023-02-03 18:58:13 +0200

    Add files via upload

--- GUITest2/User.h
+++ GUITest2/User.h
@@ -23,50 +23,17 @@ public:
         this->name = name;
     }
   
-    void addFollower(int follower)
-    {
-        followers.push_back(follower);
-    }
+    void addFollower(int follower);
     //checks if a user exists in this users followers list
-    bool exists(int val) {
-        return binarySearch(0, followers.size()-1 , val);
-    }
-
-
-    string print() {
-
-        string out = name + " (" + to_string(id) + "): {";
-        for (int i = 0; i < followers.size(); i++) {
-            out += to_string(followers[i]) + ", ";
-        }
-        out += "}";
-
-        return out;
-    }
+    bool exists(int val);
+    string print();
 
     ~User()
     {
     }
 
 private:
-    bool binarySearch(int l, int r, int val)
-    {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
-
-            if (followers[mid] == val)
-                return true;
-
-            if (followers[mid] > val)
-                return binarySearch(l, mid - 1, val);
-
-
-            return binarySearch(mid + 1, r, val);
-        }
-
-
-        return false;
-    }
+    bool binarySearch(int l, int r, int val);
     friend class graph;
 };
 
@@ -81,73 +48,11 @@ public:
         build_graph(elements);
     }
 
-    void build_graph(vector<string> elements) {
-        int current;
-        int i = 0;
-
-        for (i = 0; i < elements.size(); i++) {
-
-            if (elements[i] == "<user>") {
-                // current = customAtoi(elements[i + 2]);
-                current = stoi(elements[i + 2]);
-                users[current] = new User(current, elements[i + 5]);
-                i += 6;
-            }
-            else if (elements[i] == "<followers>") {
-                i++;
-                while (elements[i] != "</followers>") {
-                    if (elements[i] == "<id>") {
-                        users[current]->addFollower(stoi(elements[i + 1]));
-                        i += 2;
-                    }
-                    i++;
-                }
-            }
-
-        }
-    }
+    void build_graph(vector<string> elements);
     // gets most active user
-    User* getActive() {
-        User* influential = nullptr;
-
-        for (auto it : users) {
-            if (influential == nullptr) {
-                influential = it.second;
-            }
-            else {
-                if (influential->followers.size() < it.second->followers.size())
-                    influential = it.second;
-            }
-        }
-
-        return influential;
-
-    }
+    User* getActive();
     // gets most influential user
-    User* getInfluential() {
-        User* active = nullptr;
-        map <int, int> following;
-
-        for (auto it : users) {
-            for (auto p : it.second->followers) {
-                following[p]++;
-            }
-        }
-
-
-        for (auto it : following) {
-            if (active == nullptr) {
-                active = users[it.first];
-            }
-            else {
-                if (it.second > following[active->id])
-                    active = users[it.first];
-            }
-        }
-
-        return active;
-
-    }
-
-
+    User* getInfluential();
 };
+
+
