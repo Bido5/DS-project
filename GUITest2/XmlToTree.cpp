c5263b6fee073231e9d44b7e874ea35f8a5122f8
Bahnasy2001 <105355425+Bahnasy2001@users.noreply.github.com>
2023-02-03 15:54:36 +0200

    Update XmlToTree.cpp

--- GUITest2/XmlToTree.cpp
+++ GUITest2/XmlToTree.cpp
@@ -105,6 +105,121 @@ void Node::searchForTopic(Node* root, string topic, vector<string>& postsList)
 
     }
 }
+string Node::createJSON(Node* node)
+{
+    //int count_tabs = 2;
+    int count;
+    string json = "{\n";
+    json += " \"" + node->data.substr(1, node->data.length() - 2) + "\": {";
+    xmltojson(node,nullptr, json, 2);
+    json += "}";
+    return json;
+}
+void Node::xmltojson(Node* node,Node* parent, string& json, int level) {
+    
+    bool foundRepeated = true;
+    bool curlybracket = false;
+    bool squarebracket = false;
+    if (node == NULL) {
+        level-=2;
+        json += insert("\t", level);
+        //cout << "}";
+        return;
+    }
+    /* if(node->children[0]->children[0] ==NULL){ //it is a leaf
+         json += insert(" ", level);
+         json+= children[0]->data;
+     }
+     */
+    for (int i = 0; i < node->children.size(); i++)
+    {
+       vector<Node*>childrenlist;
+
+        if (node->children.size() > 1) {
+            
+            if (node->children[0]->data.compare(node->children[1]->data) == 0)
+            {
+                //if node has siblings with similar names
+                if (foundRepeated && i == 0) { 
+                    json += "\n";
+                    json += insert("\t", level);
+                    json += "\"" + node->children[i]->data.substr(1, node->children[i]->data.length() - 2) + "\":";
+                    json += "[\n";
+                    squarebracket = true;
+                    if (node->children[i]->children.size() > 1)
+                    {
+                        json += insert("\t", level);
+                        json += "{\n";
+                        curlybracket = true;
+                    }
+                    /*json += insert("\t", level);
+                    json += "{\n";*/
+                    
+                    level++;
+
+                }
+            }
+            else {
+                if (node->children[i]->children.size() > 1) {
+                    json += insert("\t", level);
+                    json += "\"" + node->children[i]->data.substr(1, node->children[i]->data.length() - 2) + "\": ";
+                    json += " {";
+                    curlybracket = true;
+                    level++;
+                }
+                else {
+                    json += insert("\t", level);
+                    json += "\"" + node->children[i]->data.substr(1, node->children[i]->data.length() - 2) + "\": ";
+                    level++;
+                }
+                
+            }
+        }
+        
+        
+        else if (node->children[i]->children.size() == 0) //leaf node
+        { 
+            if (i == node->children.size() - 1) {
+                json += "\"" + node->children[i]->data + "\",\n";
+            }
+            else {
+                json += "\"" + node->children[i]->data + "\"\n";
+            }
+           
+            
+            
+        }
+        else
+        {
+            json += "\n";
+            json += insert("\t", level);
+            json += "\"" + node->children[i]->data.substr(1, node->children[i]->data.length()-2 ) + "\":";
+            json += "{\n";
+            json += insert("\t", level);
+            curlybracket = true;
+            level++;
+            
+        }
+        
+        foundRepeated = false;
+        //cout<<json;
+        xmltojson(node->children[i],node, json, level);
+        level--;
+        if (curlybracket) {
+            json += insert("\t", level);
+            json += "}\n";
+            curlybracket = false;
+        }
+        if(squarebracket) 
+        {
+            json += insert("\t", level);
+            json += "]\n";
+            squarebracket = false;
+        }
+    }
+
+
+}
 
 vector<string> Node::getPosts() {
     return postsList;
